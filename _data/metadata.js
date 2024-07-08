201355fa207a93980c9cc2a8195d0e38c5a0d72f
Randy Brown <thatrandybrown@gmail.com>
2024-07-07 23:09:18 -0500

    content from 11ty base blog

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "That Randy Brown",
+	url: "https://example.com/",
+	language: "en",
+	description: "That Randy Brown is on Github",
+	author: {
+		name: "Randy Brown",
+		email: "thatrandybrown@gmail.com",
+		url: "https://example.com/about-me/"
+	}
+}
